4a167f3c2d2cbaf6d1ec0b747f0947f74d87b2ce
cruddell <fH6CcNRT>
2022-06-26 21:51:28 -0500

    Initial Commit

---
+++ MyResume/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyResume
+//
+//  Created by Christopher Ruddell on 6/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
